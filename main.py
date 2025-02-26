789a162d2a175823e9a9d74e6eddac5cf6cd70ff
Việt Nam <123968522+VietNam0410@users.noreply.github.com>
2025-02-26 19:55:51 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,22 +2,40 @@ import streamlit as st
 from exercises.exercise_1.preprocess import preprocess_data
 from exercises.exercise_1.train import train_model
 from exercises.exercise_1.demo import show_demo
+from exercises.exercise_2.preprocess import preprocess_mnist
+from exercises.exercise_2.train import train_mnist
+from exercises.exercise_2.demo import show_mnist_demo
 
 st.title("Machine Learning Exercises")
 
-exercise = st.sidebar.selectbox("Choose an Exercise", ["Exercise 1: Titanic Survival Prediction"])
+exercise = st.sidebar.selectbox(
+    "Chọn một Bài tập",
+    ["Exercise 1: Titanic Survival Prediction", "Exercise 2: MNIST Handwritten Digit Recognition"]
+)
 
 def display_exercise():
-    tab1, tab2, tab3 = st.tabs(["Preprocess Data", "Train Model", "Demo"])
-    
-    with tab1:
-        preprocess_data()
-    
-    with tab2:
-        train_model()
-    
-    with tab3:
-        show_demo()
+    if exercise == "Exercise 1: Titanic Survival Prediction":
+        tab1, tab2, tab3 = st.tabs(["Preprocess Data", "Train Model", "Demo"])
+        
+        with tab1:
+            preprocess_data()
+        
+        with tab2:
+            train_model()
+        
+        with tab3:
+            show_demo()
+    elif exercise == "Exercise 2: MNIST Handwritten Digit Recognition":
+        tab1, tab2, tab3 = st.tabs(["Preprocess Data", "Train Model", "Demo"])
+        
+        with tab1:
+            preprocess_mnist()
+        
+        with tab2:
+            train_mnist()
+        
+        with tab3:
+            show_mnist_demo()
 
-if exercise == "Exercise 1: Titanic Survival Prediction":
+if exercise in ["Exercise 1: Titanic Survival Prediction", "Exercise 2: MNIST Handwritten Digit Recognition"]:
     display_exercise()
